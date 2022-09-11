a19e65b5e1e71c1d8831f296a25a4928e48016df
Gokul Govinda <87459046+gokulgovinda@users.noreply.github.com>
2022-09-11 23:52:09 +0530

    Update Mainclass.java

--- Mainclass.java
+++ Mainclass.java
@@ -6,7 +6,8 @@ public static void main(String[] args) {
  
      System.out.println("Enter a String: ");
      String str = sc.next();
-      String org_str = str;
+     String org_str = str;
+     
      String rev = "";
      int len =str.length();
      for(int i=len-1;i>=0;i--){
